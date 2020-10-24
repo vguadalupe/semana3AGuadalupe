b6056a4975020f143ef7ee1e809d2014e3c73f7c
vguadalupe <44405688+vguadalupe@users.noreply.github.com>
2020-10-23 21:15:37 -0500

    Add project files.

---
+++ semana3AGuadalupe/semana3AGuadalupe/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace semana3AGuadalupe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrirVentana2_Clicked(object sender, EventArgs e)
+        {
+
+            string user = txtUser.Text;
+            string pass = txtPassword.Text;
+
+            string user_val = "uisrael";
+            string pass_val = "uisrael2020";
+
+            if (user == user_val && pass==pass_val)
+            {
+                await Navigation.PushAsync(new Ventana2(user, pass));
+            }
+            else
+            {
+                await DisplayAlert("Login Incorrecto", "Datos incorrectos", "Volver");
+            }
+            
+
+            
+        }
+    }
+}
---
+++ semana3AGuadalupe/semana3AGuadalupe/Ventana2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana3AGuadalupe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ventana2 : ContentPage
+    {
+        
+        public Ventana2(string user, string pass)
+        {
+            InitializeComponent();
+
+            lbluser.Text = user;
+            txtPass.Text = pass;
+        }
+    }
+}
